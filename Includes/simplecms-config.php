ba01e984a3aed00abd0a634d20f2f26a67935935
Bhami <jainbhami@gmail.com>
2016-02-17 11:48:39 -0600

    initial commit

---
+++ Includes/simplecms-config.php
@@ -0,0 +1,9 @@
+<?php
+    define('DB_NAME', 'simplecms393');
+    define('DB_USER', 'simplecmsuser393');
+    define('DB_PASSWORD', 'NFtJz-df#mCi');
+    define('DB_HOST', 'localhost');
+
+    define('DEFAULT_ADMIN_USERNAME', 'admin');
+    define('DEFAULT_ADMIN_PASSWORD', 'priya123');
+?>
